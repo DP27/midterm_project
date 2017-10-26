2e76a5df6088625f44c0015ad917df98afc3b081
Jasper Walton <jasper.lyn.walton@gmail.com>
2017-10-26 18:41:32 +0000

    updates table columns

--- db/migrations/20160712210838_create_users_table.js
+++ db/migrations/20160712210838_create_users_table.js
@@ -1,7 +1,9 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('users', function (table) {
-    table.increments();
+    table.increments('id');
     table.string('name');
+    table.string('email');
+    table.string('password');
   });
 };
 
