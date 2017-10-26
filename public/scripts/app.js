97fe2d1885ce4610ed28abd44b90b6c79674ced3
Dishant <dishantpatel27@gmail.com>
2017-10-26 01:11:23 +0000

    implemented google maps query

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -8,3 +8,50 @@
 //     }
 //   });;
 // });
+
+
+$(document).ready(function getlocation(){
+    
+      $('#location').on('click',function(){
+          let locationText = $(document).find('.location-placeholder').val();
+          getLatLng(locationText);
+      })
+
+})
+
+function getLatLng(locationText){
+    var geocoder = new google.maps.Geocoder();
+    var address = locationText;
+ 
+    if (geocoder) {
+       geocoder.geocode({ 'address': address }, function (results, status) {
+          if (status == google.maps.GeocoderStatus.OK) {
+            let lat = results[0].geometry.location.lat();
+            let lng = results[0].geometry.location.lng();
+            
+             console.log(results[0].geometry.location.lat());
+             console.log(results[0].geometry.location.lng());
+             let arr = [lat,lng];
+             retlatlng(arr);
+             return arr;
+          }
+          else {
+             console.log("Geocoding failed: " + status);
+          }
+       });
+    }    
+
+}
+
+function retlatlng(arr){
+    if(arr[0]){
+        let dir = {lat: arr[0],lng: arr[1]};
+        console.log(dir);
+        return dir;
+    }
+
+}
+
+
+
+
