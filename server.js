ca1238159440a68ae9b43bdbbf18b08b094cc34a
Dishant <dishantpatel27@gmail.com>
2017-10-26 20:22:57 +0000

    added time and date in the form field(create_event)

--- server.js
+++ server.js
@@ -16,8 +16,6 @@ const knexLogger  = require('knex-logger');
 
 // Seperated Routes for each Resource
 const usersRoutes = require("./routes/users");
-//const eventsRoutes = require("./routes/events");
-
 
 // Load the logger first so all (static) HTTP requests are logged to STDOUT
 // 'dev' = Concise output colored by response status for development use.
@@ -70,7 +68,7 @@ app.post("/:id", (req, res) => {
   } else if (!req.body.event_name) {
     res.status(400).send("Please input an event name.");
   //} else if (!/* DATE TIME INPUTS */){
-    //res.status(400).send("Please input at least one date and time.")
+  //  res.status(400).send("Please input at least one date and time.");
   } else {
     let id = generateRandomString(20);
     // CHECK DATABASE IF ID EXISTS ALREADY
@@ -100,3 +98,37 @@ app.post("/:id/delete", (req, res) => {
 app.listen(PORT, () => {
   console.log("Example app listening on port " + PORT);
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
