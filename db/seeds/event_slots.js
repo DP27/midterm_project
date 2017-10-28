502a745a760e4c9e1c2113985e6597daa68ebe65
Jasper Walton <jasper.lyn.walton@gmail.com>
2017-10-28 00:12:19 +0000

    adds seed data

--- db/seeds/event_slots.js
+++ db/seeds/event_slots.js
@@ -2,11 +2,17 @@ exports.seed = function(knex, Promise) {
   return knex('event_slots').del()
     .then(function () {
       return Promise.all([
-        knex('event_slots').insert({date: 12062017,
+        knex('event_slots').insert({id: 1,
+                                    event_id: 90283,
+                                    date: 12062017,
                                     time: 0830}),
-        knex('event_slots').insert({date: 12102017,
+        knex('event_slots').insert({id: 2,
+                                    event_id: 9081,
+                                    date: 12102017,
                                     time: 0645}),
-        knex('event_slots').insert({date: 12122017,
+        knex('event_slots').insert({id: 3,
+                                    event_id: 076532,
+                                    date: 12122017,
                                     time: 0315})
       ]);
     }).then(function (event_slots) {
