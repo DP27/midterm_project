8b4c5889e4811793d09636319fc114bfc3a81059
Jasper Walton <jasper.lyn.walton@gmail.com>
2017-10-28 22:02:44 +0000

    initial commit, gets event title, description, location

---
+++ routes/load_event.js
@@ -0,0 +1,20 @@
+"use strict";
+
+const express = require('express');
+const router  = express.Router();
+
+module.exports = (knex) => {
+
+  router.get("/:id", (req, res) => {
+
+    knex
+      .select("title", "description", "location")
+      .from("events")
+      .where("events.id", req.params.id)
+      .then((results) => {
+        res.json(results);
+    });
+  });
+
+  return router;
+}
