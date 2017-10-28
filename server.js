16b00e8b603eb46e75740be497ccd52ae7996c64
Jasper Walton <jasper.lyn.walton@gmail.com>
2017-10-28 23:11:04 +0000

    debugging

--- server.js
+++ server.js
@@ -150,6 +150,7 @@ app.get("/:id", (req, res) => {
 });
 
 app.post('/:id', (req, res) => {
+  console.log("post req server side:",req.body.event_slots);
   const eventSlotStrs = typeof req.body.event_slots === 'string' ? [req.body.event_slots] : req.body.event_slots;
   const eventSlots = eventSlotStrs.map(s => parseInt(s));
 
