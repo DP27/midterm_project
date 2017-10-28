8a99d69845bb5a95031f37d83be8d42ceb418fb0
Jasper Walton <jasper.lyn.walton@gmail.com>
2017-10-28 19:30:14 +0000

    adjusts seed data

--- db/seeds/3_event_slots.js
+++ db/seeds/3_event_slots.js
@@ -5,7 +5,7 @@ exports.seed = function(knex, Promise) {
         knex('event_slots').insert({id: 1,
                                     event_id: 90283,
                                     date: "2017/12/06",
-                                    time: "0830"}),
+                                    time: "1800"}),
         knex('event_slots').insert({id: 2,
                                     event_id: 90283,
                                     date: "2017/12/06",
@@ -17,7 +17,7 @@ exports.seed = function(knex, Promise) {
         knex('event_slots').insert({id: 4,
                                     event_id: 076532,
                                     date: "2017/12/12",
-                                    time: "03:15"})
+                                    time: "0315"})
       ]);
     }).then(function (event_slots) {
       // associate event_slots to other database tables
