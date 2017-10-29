6219099345956958a1708686be9c8eb78d61a173
Rachael Topp <rachaeltopp@Rachaels-MacBook-Air.local>
2017-10-29 16:47:17 -0400

    clean up

--- public/scripts/view.js
+++ public/scripts/view.js
@@ -4,53 +4,55 @@ function createSlot(event_slots) {
   let time = slot.time
   let slot_id = slot.id
 
-  let slotHTML = $(".slots").append(
-    $("<div>").addClass("checkbox")
-      .append($("<label>").text(date + " " + time)
-      .append($("<input>")
-      .attr({type: "checkbox", name: "event_slots", value: slot_id}))))         
-  return slotHTML
+  let inhtml = `<div class = "checkbox" data-like=${slot_id}>
+  <label for="checkbox"> ${date}${time}
+  <input type="checkbox" name = "event_slots" value=${slot_id}>
+  </label>
+  </div>`
+
+  let slotHTML = $(".slots").append(inhtml);
+  return slotHTML;
 }
 
-function createNames(name) {
-  let namesHTML = $(".checkbox").append(
+function createNames(name, slotId) {
+  var namesHTML = $(".checkbox").filter(`[data-like='${slotId}']`).append(
                   $("<p>").text(name));
   return namesHTML;
 };
 
 function eventTitle(title) {
   let eventHTML = $(".event-info").append(
-                  ($("<h1>").text(title)))
+                  $("<h1>").text(title));
   return eventHTML;
 };
 
 function eventDescription(description) {
   let eventHTML = $(".event-info").append(
-                  ($("<p>").text(description)))
+                  $("<p>").text(description));
   return eventHTML;
 }
 
-function eventLocation(location){
+function eventLocation(location) {
   var geocoder = new google.maps.Geocoder();
   var address = location;
 
   if (geocoder) {
-      geocoder.geocode({'address': address}, function (results, status) {
-        if (status == google.maps.GeocoderStatus.OK) {
-          let lat = results[0].geometry.location.lat();
-          let lng = results[0].geometry.location.lng();
-          var uluru = {lat: lat, lng: lng};
-          var map = new google.maps.Map(document.getElementById('map'), {
-            zoom: 13,
-            center: uluru
-          })
-          var marker = new google.maps.Marker({
-            position: uluru,
-            map: map
-          }) 
-          return
-        }
-      })
+    geocoder.geocode({'address': address}, function (results, status) {
+      if (status == google.maps.GeocoderStatus.OK) {
+        let lat = results[0].geometry.location.lat();
+        let lng = results[0].geometry.location.lng();
+        var uluru = {lat: lat, lng: lng};
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 13,
+          center: uluru
+        })
+        var marker = new google.maps.Marker({
+          position: uluru,
+          map: map
+        }) 
+        return
+      }
+    })
   }
 }
 
@@ -63,8 +65,8 @@ $(() => {
     method: "GET",
     url: "/api/event_slots/" + eventId
   }).done((event_slots) => {   
-      for(slot of event_slots) {
-      createSlot(slot);
+    for(slot of event_slots) {
+    createSlot(slot);
     }
   })  
 
@@ -85,16 +87,10 @@ $(() => {
     method: "GET",
     url: "/api/votes/" + eventId
   }).done((users) => {
-    const arr = {};
-
-    users.forEach(function(user) {
-      arr[user.slot_id] = arr[user.slot_id] || [];
-      arr[user.slot_id].push(user); 
-    });
-    for(element in arr) {
-      console.log(arr[element]);
+    for(user of users) {
+      let name = user.name;
+      let slotId = user.slot_id;
+      createNames(name,slotId);
     }
-    //console.log(arr);
   })
 })
-//createNames(user.name);
