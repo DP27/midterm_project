f724b06c5146417de89aec0e08dfa52549b2b925
Jasper Walton <jasper.lyn.walton@gmail.com>
2017-10-29 21:37:40 +0000

    some error handling, user has to input a name and email to vote

--- server.js
+++ server.js
@@ -151,6 +151,13 @@ app.post('/:id', (req, res) => {
   const eventSlotStrs = typeof req.body.event_slots === 'string' ? [req.body.event_slots] : req.body.event_slots;
   const eventSlots = eventSlotStrs.map(s => parseInt(s));
 
+if (!req.body.name) {
+    res.status(400).send("Please input a name.");
+  } else if (!req.body.email) {
+    res.status(400).send("Please input an email.");
+  //} else if (!slot_id) {
+    //res.status(400).send("Please vote at least once.");
+  } else {
   knex('users')
   .insert({name: req.body.name, email: req.body.email})
   .returning('id')
@@ -172,6 +179,7 @@ app.post('/:id', (req, res) => {
   }).catch((e) => {
     res.status(500).send();
   });
+  }
 })
 
 app.post("/:id/delete", (req, res) => {
@@ -183,37 +191,3 @@ app.post("/:id/delete", (req, res) => {
 app.listen(PORT, () => {
   console.log("Example app listening on port " + PORT);
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
