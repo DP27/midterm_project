3e1296f25faba92ce1f8a833515f6c38706953ef
Jasper Walton <jasper.lyn.walton@gmail.com>
2017-10-29 01:44:59 +0000

    creates join table

---
+++ routes/votes.js
@@ -0,0 +1,23 @@
+"use strict";
+
+const express = require('express');
+const router  = express.Router();
+
+module.exports = (knex) => {
+
+  router.get("/:id", (req, res) => {
+
+    knex
+        .select("name", "slot_id", "date", "time")
+        .from("users")
+        .innerJoin("votes", "users.id", "user_id")
+        .innerJoin("event_slots", "slot_id", "event_slots.id")
+        .where("event_id", req.params.id)
+
+      .then((results) => {
+        res.json(results);
+    });
+  });
+
+  return router;
+}
