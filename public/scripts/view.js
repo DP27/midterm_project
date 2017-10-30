0db12115da8f005d554b36030a519c0d3681ea41
Rachael Topp <rachaeltopp@Rachaels-MacBook-Air.local>
2017-10-30 10:19:01 -0400

    took off seconds from time

--- public/scripts/view.js
+++ public/scripts/view.js
@@ -9,9 +9,10 @@ function createSlot(event_slots) {
   let betterDate = newDate.toString().replace(/00:00:00|GMT-0400|GMT-0500|EST|EDT|\(|\)/g,"");
 
   let time = slot.time
+  const newTime = time.substring(0,5);
   let slot_id = slot.id
   let inhtml = `<div class = "checkbox" data-like=${slot_id}>
-  <label for="checkbox"> ${betterDate}${time}
+  <label for="checkbox"> ${betterDate}${newTime}
   <input type="checkbox" name="event_slots" value=${slot_id}>
   </label>
   </div>`
